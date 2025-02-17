80b746fa9dc8b553da90d5b4168a13e5862c7cb4
Chrislight879 <christopher.vanegas@catolica.edu.sv>
2025-02-17 00:46:27 -0600

    todo

---
+++ Esrilos.js
@@ -0,0 +1,8 @@
+
+var typing = new Typed (".typing" ,{
+strings : ["a Database Administrator","a Junior Programmer","a Web Designer", " an Editor"],
+typeSpeed: 100,
+backSpeed: 50,
+loop: true
+});
+
